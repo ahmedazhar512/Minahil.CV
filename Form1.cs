c0d2ea74164c63f5cb0704f00fdf69c6c72b87b9
ahmedazhar512 <102924545+ahmedazhar512@users.noreply.github.com>
2024-04-05 15:55:32 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="laiba"&& textBox2.Text=="1234")
+            {
+                
+
+                new Form2().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("the username or password is incorrect, try again");
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox1.Focus();
+
+                    
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
